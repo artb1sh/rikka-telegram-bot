a2a9571c54adeb945833b3f20988a35d15e91d2d
lina <yjcjxtr@gmail.com>
2019-09-22 17:29:27 +0500

    heroku

--- rikkabot.py
+++ rikkabot.py
@@ -79,7 +79,7 @@ dp.add_handler(CommandHandler("help", help))
 
 
 # Starting bot
-port = os.environ.get('PORT', 8443)
+port = int(os.environ.get('PORT', 8443))
 updater.start_webhook(clean=True, bootstrap_retries=0, port=port, url_path=key,
                       cert='cert.pem', key='private.key')
 print("=====================\nUp and running!\n")
